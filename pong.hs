fa5a1f9913855ebcd028336b74eec0478597060f
Mark Santolucito <mark.santolucito@yale.edu>
2014-11-24 14:36:37 -0500

    a passable flappy clone

--- pong.hs
+++ pong.hs
@@ -11,31 +11,52 @@ import qualified FRP.Helm.Sample as S
 
 -- MODEL
 data Character = Character { x :: Double,
-                             energy :: Double}
-			     --y :: Int,
+                             energy :: Double,
+			     npc :: Bool}
 			     --vx :: Double,
 			     --vy :: Double,
 			    -- dir :: String }
 
-bird = Character 0 750
+bird = Character 300 750 False
+obstacle1 = Character 800 500 True
+obstacle2 = Character 1000 200 True
 
 
 -- UPDATE -- ("m" is for Mario)
 
-step :: Bool -> Character -> Character
-step keys bird  | energy bird > 751 = Character ((x bird)-1) (750)
-                | energy bird < 0 = Character ((x bird)-1) (0)  
-                | keys == True = Character ((x bird)+1) ((energy bird)-3)
-                | keys == False = Character ((x bird)-0.5) ((energy bird)+1.5)
+step :: Bool -> [Character] -> [Character]
+step keys cs = map (update keys) cs
+
+update keys c | npc c == True = (npcUpdate c) True 
+              | npc c == False = (playerUpdate keys c) False
+
+npcUpdate :: Character -> Bool -> Character
+npcUpdate c
+    | x c < 0 = Character (800) (energy c)
+    | x c >= 0 = Character ((x c)-1) (energy c)
+
+playerUpdate :: Bool -> Character -> Bool -> Character
+playerUpdate keys c
+    | energy c > 751 = Character ((x c)+0.5) (750)
+    | energy c < 0 = Character ((x c)+0.5) (0)
+    | keys == True = Character ((x c)-1) ((energy c)-3)
+    | keys == False = Character ((x c)+0.5) ((energy c)+1.5)
 
 
 -- DISPLAY
 
 --need to scale to windows dimensions
-render :: Character -> (Int,Int) -> Element
-render c (w,h) = collage w h [rect ((*2) $ energy c) 25 |> filled red]
+render :: [Character] -> (Int,Int) -> Element
+render cs (w,h) = collage w h $ concat (map my_collage cs)
 
+my_collage :: Character -> [Form]
+my_collage c
+      | npc c == False = (healthBar c) ++ (player c)
+      | npc c == True = obs c
 
+healthBar c = [rect ((*2) $ energy c) 25 |> filled green]
+player c = [move (400, x c) $ filled white $ square 50]
+obs c = [move (x c , energy c) $ filled red $ rect 25 100]
 
 -- INPUT
 runAt c s = let x = lift2 (,) c s
@@ -50,5 +71,5 @@ main = do
     run config $ render <~ stepper ~~ Window.dimensions
   
   where
-    config = defaultConfig { windowTitle = "Helm - Mario" }
-    stepper = foldp step bird input
+    config = defaultConfig { windowTitle = "Helm - Flappy" }
+    stepper = foldp step [bird,obstacle1,obstacle2] input
